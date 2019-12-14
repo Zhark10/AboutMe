a55a3376b02add4921dfa53470eff07db0655410
Zhark10 <arkady.zharavin@citronium.biz>
2019-12-14 03:04:06 +0300

    added simple audio player

--- src/Content/Main/Main.tsx
+++ src/Content/Main/Main.tsx
@@ -2,12 +2,18 @@ import React, { useEffect } from 'react';
 import './Main.css';
 import { handleScroll } from '../../utils/scroll';
 import { game } from './game';
+import { initPage } from '../../utils/player';
 
 const Main: React.FC = () => {
+
+    useEffect(()=>{
+        initPage()   
+    }, [])
 
     return (
         <div className="first-content">
             <div className="scroll-down" onClick={handleScroll} data-target-id="target2"></div>
+            <canvas id="renderer"></canvas>
         </div>
     );
 }
