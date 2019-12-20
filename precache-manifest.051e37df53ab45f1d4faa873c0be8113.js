e7e66154435ade53bc7fdcd4be0fa60171bcbfed
Zhark10 <arkady.zharavin@citronium.biz>
2019-12-21 01:02:35 +0300

    Updates

---
+++ precache-manifest.051e37df53ab45f1d4faa873c0be8113.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ef73775f2fe5f3eae1125064a66a9c",
+    "url": "/about_me/index.html"
+  },
+  {
+    "revision": "1e05264c698a66e76db6",
+    "url": "/about_me/static/css/main.fb89e095.chunk.css"
+  },
+  {
+    "revision": "0e254f27ff70e0c6df5c",
+    "url": "/about_me/static/js/2.082aa807.chunk.js"
+  },
+  {
+    "revision": "1e05264c698a66e76db6",
+    "url": "/about_me/static/js/main.ade6fad7.chunk.js"
+  },
+  {
+    "revision": "6e7480a3fc6c00e7fb49",
+    "url": "/about_me/static/js/runtime-main.9fe25567.js"
+  },
+  {
+    "revision": "2861a7961424d6556d72b0b73140922c",
+    "url": "/about_me/static/media/AUSLÄNDER.2861a796.mp3"
+  },
+  {
+    "revision": "ec10a34082c7d0f3a28ac3d1431cce42",
+    "url": "/about_me/static/media/Alter_Ego.ec10a340.mp3"
+  },
+  {
+    "revision": "4cab55a43c473eb8585af9ac380de9b6",
+    "url": "/about_me/static/media/Ghinzu.4cab55a4.mp3"
+  },
+  {
+    "revision": "ab8aa965eabf7c05e5f1297d7f8965f6",
+    "url": "/about_me/static/media/JetSex.ab8aa965.mp3"
+  },
+  {
+    "revision": "7ee7904d4356f63027076e6def5d0e4d",
+    "url": "/about_me/static/media/citronium.7ee7904d.png"
+  },
+  {
+    "revision": "0ea36c120c5d4339dcbe39a09ed43f1f",
+    "url": "/about_me/static/media/face.0ea36c12.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Zhark10/about_me/precache-manifest.4816761bd227f2988e50fb2ed9069765.js"
+  "/about_me/precache-manifest.051e37df53ab45f1d4faa873c0be8113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zhark10/about_me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/about_me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
