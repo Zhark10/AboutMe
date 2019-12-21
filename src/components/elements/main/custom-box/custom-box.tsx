dcf7944c5339d220af66fc2cd1165c2d418881e2
Zhark10 <arkady.zharavin@citronium.biz>
2019-12-22 02:00:31 +0300

    added my custom box

---
+++ src/components/elements/main/custom-box/custom-box.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './custom-box.css';
+import Cable from '../../../shared/cable';
+
+const CustomBox = () => (
+    <div className="my-custom-box">
+        <Cable length={60} />
+        <div className="custom-box" >
+
+        </div>
+    </div>
+);
+
+export default CustomBox;
+
--- src/pages/AboutMePage/content/Main/Main.tsx
+++ src/pages/AboutMePage/content/Main/Main.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './Main.css';
 import { handleScroll } from '../../../../utils/scroll';
-import Cable from '../../../../components/shared/cable';
-
+import CustomBox from '../../../../components/elements/main/custom-box/custom-box';
 
 const Main: React.FC = () => {
     return (
         <div className="first-content">
-            <div className="my-custom-box">
-                <Cable length={60} />
-                {/* <CustomBox /> */}
-            </div>
+            <CustomBox />
             <div className="scroll-down" onClick={handleScroll} data-target-id="target2"></div>
         </div>
     );
